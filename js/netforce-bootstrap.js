238be6e758aaac95713b012bd2f991ba7861869e
Bruno Goncalves <bugotech@gmail.com>
2017-07-26 13:51:34 -0300

    v2.5

---
+++ js/netforce-bootstrap.js
@@ -0,0 +1,16 @@
+/**
+ * ZJS 1.0.0
+ * Date: 2017-07-26 13:49:38
+ */
+
+/*---  ZJS/NFBS.ZJS  ---*/
+(function ($) {
+
+    $(document).ready(function () {
+
+        // Auto Focus
+        $('.focus:first').focus();
+
+    });
+
+})(window.jQuery);
